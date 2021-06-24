a66a6f317cb7eb1d9290440456ccf08ba47f5a38
Nicki-Di <78273681+Nicki-Di@users.noreply.github.com>
2021-06-24 22:10:34 +0430

    pagination style added.

--- Main Page/MainPage.js
+++ Main Page/MainPage.js
@@ -16,21 +16,76 @@ function showSlides(n) {
     let slides = document.getElementsByClassName("slides");
     let dots = document.getElementsByClassName("dot");
     if (n > slides.length) {
-        slideIndex = 1
+        slideIndex = 1;
     }
     if (n < 1) {
-        slideIndex = slides.length
+        slideIndex = slides.length;
     }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+        dots[i].classList.remove("active-dot");
     }
     slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+    dots[slideIndex - 1].classList.add("active-dot");
 }
 
 setInterval(plusSlides, 7000, 1); // every 7 sec
 
 // slideshow - end
+
+// pagination - start
+let currentPage = "page1";
+window.onload = function () {
+    disPage()
+}
+
+function disPage() {
+    if (currentPage == "page1") {
+        let button = document.getElementById("prev-page");
+        button.classList.add("disable-page")
+    } else {
+        let button = document.getElementById("prev-page");
+        button.classList.remove("disable-page");
+    }
+    if (currentPage == "page3") {
+        let button = document.getElementById("next-page");
+        button.classList.add("disable-page")
+    } else {
+        let button = document.getElementById("next-page");
+        button.classList.remove("disable-page");
+    }
+}
+
+function changePage(page) {
+    currentPage = page;
+    disPage();
+    let otherButtons = document.getElementsByClassName("pagination-button");
+    for (let i = 0; i < otherButtons.length; i++) {
+        otherButtons[i].classList.remove("active-page")
+    }
+    let clickedButton = document.getElementById(page);
+    clickedButton.classList.add("active-page")
+}
+
+function prevPage() {
+    if (currentPage != "page1") {
+        let tmp = currentPage[4];
+        tmp = parseInt(tmp) - 1;
+        currentPage = "page" + tmp.toString();
+        changePage(currentPage);
+    }
+
+}
+
+function nextPage() {
+    if (currentPage != "page3") {
+        let tmp = currentPage[4];
+        tmp = parseInt(tmp) + 1;
+        currentPage = "page" + tmp.toString();
+        changePage(currentPage);
+    }
+}
+
+// pagination - end
