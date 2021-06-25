46a151b55a7bb1e02820ed77d201262b01ed1a10
Nicki-Di <78273681+Nicki-Di@users.noreply.github.com>
2021-06-25 04:36:06 +0430

    range slider completed.

--- Main Page/MainPage.js
+++ Main Page/MainPage.js
@@ -232,41 +232,41 @@ function nextPage() {
 
 
 // Rane slider - start
-var inputLeft = document.getElementById("input-left");
-var inputRight = document.getElementById("input-right");
+let inputLeft = document.getElementById("input-left");
+let inputRight = document.getElementById("input-right");
 
-var thumbLeft = document.querySelector(".slider > .thumb.left");
-var thumbRight = document.querySelector(".slider > .thumb.right");
-var range = document.querySelector(".slider > .range");
+let thumbLeft = document.querySelector(".slider > .thumb.left");
+let thumbRight = document.querySelector(".slider > .thumb.right");
+let range = document.querySelector(".slider > .range");
 
 function setLeftValue() {
-    var _this = inputLeft,
+    let _this = inputLeft,
         min = parseInt(_this.min),
         max = parseInt(_this.max);
 
     _this.value = Math.min(parseInt(_this.value), parseInt(inputRight.value) - 1);
 
-    var percent = ((_this.value - min) / (max - min)) * 100;
+    let percent = ((_this.value - min) / (max - min)) * 100;
 
     thumbLeft.style.left = percent + "%";
     range.style.left = percent + "%";
-    document.getElementById("input-left-output").innerHTML = document.getElementById("input-left").value;
+    document.getElementById("input-left-output").innerHTML = inputLeft.value;
 }
 
 setLeftValue();
 
 function setRightValue() {
-    var _this = inputRight,
+    let _this = inputRight,
         min = parseInt(_this.min),
         max = parseInt(_this.max);
 
     _this.value = Math.max(parseInt(_this.value), parseInt(inputLeft.value) + 1);
 
-    var percent = ((_this.value - min) / (max - min)) * 100;
+    let percent = ((_this.value - min) / (max - min)) * 100;
 
     thumbRight.style.right = (100 - percent) + "%";
     range.style.right = (100 - percent) + "%";
-    document.getElementById("input-right-output").innerHTML = document.getElementById("input-right").value;
+    document.getElementById("input-right-output").innerHTML = inputRight.value;
 }
 
 setRightValue();
