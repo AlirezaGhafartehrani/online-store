de5b50e4a39a3f84dd9c76caa88b7a1f9c312cd0
Nicki-Di <78273681+Nicki-Di@users.noreply.github.com>
2021-06-27 01:06:59 +0430

    server development

--- Main Page/server.js
+++ Main Page/server.js
@@ -1,5 +1,19 @@
 let http = require('http');
+let mysql = require('mysql');
 
+let con = mysql.createConnection({
+    host: "localhost",
+    user: "webAdmin",
+    password: "AUgEPslVcgf9RI6z"
+});
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    con.query("CREATE DATABASE web_db", function (err, result) {
+        if (err) throw err;
+        console.log("Database created");
+    });
+});
 http.createServer(function (request, response) {
     if (request.method === 'GET') {
         response.writeHead(200, {'Content-Type': 'text/plain', 'Access-Control-Allow-Origin': '*'});
