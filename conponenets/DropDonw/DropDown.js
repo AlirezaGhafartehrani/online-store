6941735562e85c67851e2dca8c77e37cac5c8e30
AlirezaGhafartehrani <alirezatehrani@aut.ac.ir>
2021-07-20 08:44:17 +0430

    Add functionality to dorpdonw

---
+++ conponenets/DropDonw/DropDown.js
@@ -0,0 +1,9 @@
+// define Elements:
+
+const logOutBtn = document.getElementById("logOutBtn");
+
+// event listeners
+
+logOutBtn.addEventListener("click", function logout(e) {
+  // logout and navigate some where else
+});
